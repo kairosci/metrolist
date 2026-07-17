275ffb92094f045e23be5f8d997040b752b6a61f
nyxiereal <98595166+nyxiereal@users.noreply.github.com>
2026-07-17 13:25:35 +0200

    chore: update deps, remove eq button from volume slider

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -213,7 +213,6 @@ android {
     kotlin {
         jvmToolchain(21)
         compilerOptions {
-            freeCompilerArgs.add("-Xannotation-default-target=param-property")
             jvmTarget.set(JvmTarget.JVM_21)
         }
     }
